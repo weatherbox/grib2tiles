7f255a57ee4fbdaa2e9964b4468a31c0959866ce
tattii <yuta.tatti@gmail.com>
2016-09-01 02:12:02 +0900

    thinout

--- grib2tiles.py
+++ grib2tiles.py
@@ -40,49 +40,83 @@ def msm_to_tiles(file):
             pdt['parameter_category'],
             pdt['parameter_number'])
             
-        print ref_time_str, forecast_time_str, level, element
         directory = '/'.join(['tiles', ref_time_str, forecast_time_str, level, element])
 
         if level == 'surface' and (element == 'UGRD' or element == 'VGRD'):
             to_tile(directory, data, bin_RED, ni=481, nj=505, level=2)
+            to_tile(directory, data, bin_RED, ni=481, nj=505, level=1, thinout=1)
             
         elif element == 'UGRD' or element == 'VGRD': # upper
             to_tile(directory, data, bin_RED, ni=241, nj=253, level=1)
 
 
-def to_tile(dir, data, bin_RED, ni, nj, level=1):
+def to_tile(dir, data, bin_RED, ni, nj, level=1, thinout=0):
     ntile = 2 ** level
-    tni = (ni - 1) / ntile + 1
-    tnj = (nj - 1) / ntile + 1
 
     directory = dir + '/' + str(level)
     if not os.path.exists(directory):
         os.makedirs(directory)
+    print directory
 
-    for ty in range(0, ntile):
-        for tx in range(0, ntile):
-            tile_data = []
-            lx1 = (tni - 1) * tx
-            lx2 = (tni - 1) * (tx + 1) + 1
-            length = (lx2 - lx1) * 12
-            format_row = 'r' + str(length)
+    if thinout == 0:
+        tni = (ni - 1) / ntile + 1
+        tnj = (nj - 1) / ntile + 1
 
-            for y in range(0, tnj):
-                base_y = ni * (ty + y)
-                bx1 = int(math.floor((base_y + lx1) * 12 / 8.))
-                bx2 = int(math.ceil((base_y + lx2) * 12 / 8.))
-                d = data[bx1:bx2]
+        for ty in range(0, ntile):
+            for tx in range(0, ntile):
+                tile_data = []
+                lx1 = (tni - 1) * tx
+                lx2 = (tni - 1) * (tx + 1) + 1
+                length = (lx2 - lx1) * 12
+                format_row = 'r' + str(length)
 
-                if ((base_y + lx1) * 12) % 8 == 4:
-                    tile_data.extend(list(bitstruct.unpack('p4' + format_row, d)))
-                else:
-                    tile_data.extend(list(bitstruct.unpack(format_row, d)))
+                for y in range(0, tnj):
+                    base_y = ni * (ty + y)
+                    bx1 = int(math.floor((base_y + lx1) * 12 / 8.))
+                    bx2 = int(math.ceil((base_y + lx2) * 12 / 8.))
+                    d = data[bx1:bx2]
 
-            bin_tile_data = bitstruct.pack(format_row * tnj, *tile_data)
+                    if ((base_y + lx1) * 12) % 8 == 4:
+                        tile_data.extend(list(bitstruct.unpack('p4' + format_row, d)))
+                    else:
+                        tile_data.extend(list(bitstruct.unpack(format_row, d)))
 
-            f = open(directory + '/%d_%d.bin' % (tx, ty), 'w')
-            f.write(bin_RED + bin_tile_data)
-            f.close()
+                bin_tile_data = bitstruct.pack(format_row * tnj, *tile_data)
+
+                f = open(directory + '/%d_%d.bin' % (tx, ty), 'w')
+                f.write(bin_RED + bin_tile_data)
+                f.close()
+
+    elif thinout > 0:
+        thinout_level = 2 ** thinout
+        tni = (ni - 1) / thinout_level / ntile + 1
+        tnj = (nj - 1) / thinout_level / ntile + 1
+
+        for ty in range(0, ntile):
+            for tx in range(0, ntile):
+                tile_data = []
+                lx1 = (tni - 1) * thinout_level * tx
+                lx2 = (tni - 1) * thinout_level * (tx + 1) + 1
+                l = tni - 1
+                format_row = ('u12' + 'p12' * (thinout_level - 1)) * l + 'u12'
+
+                for y in range(0, tnj):
+                    base_y = ni * (thinout_level * ty + y)
+                    bx1 = int(math.floor((base_y + lx1) * 12 / 8.))
+                    bx2 = int(math.ceil((base_y + lx2) * 12 / 8.))
+                    d = data[bx1:bx2]
+
+                    if ((base_y + lx1) * 12) % 8 == 4:
+                        tile_data.extend(list(bitstruct.unpack('p4' + format_row, d)))
+                    else:
+                        tile_data.extend(list(bitstruct.unpack(format_row, d)))
+
+                bin_tile_data = bitstruct.pack('u12' * tni * tnj, *tile_data)
+
+                f = open(directory + '/%d_%d.bin' % (tx, ty), 'w')
+                f.write(bin_RED + bin_tile_data)
+                f.close()
+
 
 if __name__ == '__main__':
     file = sys.argv[1]
