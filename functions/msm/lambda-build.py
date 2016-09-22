6305266254134f58dbd50ac1a6bfe213a2e50993
tattii <yuta.tatti@gmail.com>
2016-09-22 23:02:39 +0900

    add lambda-build.py

---
+++ functions/msm/lambda-build.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+import datetime
+
+
+if __name__ == '__main__':
+    date = datetime.datetime.now().strftime('%Y%d%m-%H%M%S')
+    package = 'lambda-grib2tiles-msm-' + date + '.zip'
+    shutil.copy('lambda-numpy.zip', package)
+
+    for file in ['bitstruct.py', 'grib2tiles.py', 'msm.py', 'main.py']:
+        os.system('zip -9 ' + package + ' ' + file) 
+
+    print 'created deploy package ' + package
